221ebfb471f7b7823b39e7fd7bf10ba1df13270a
CatalinaRpoS <catrestreposa@unal.edu.co>
2024-02-26 12:29:15 -0500

    Change order on the dataframe

--- clean_data.py
+++ clean_data.py
@@ -14,8 +14,32 @@ def load_data(input_file):
 def create_key(df, n):
     """Cree una nueva columna en el DataFrame que contenga el key de la columna 'text'"""
 
+    new_df = df.copy()
     df = df.copy()
 
+    """Con este paso, se aplica el algoritmo de Porter para encontrar la raíz de cada palabra"""
+    # Copie la columna 'text' a la columna 'stem'
+    df["stem"] = new_df["text"]
+    df["stem"] = (
+        df["stem"]
+        # Remueva los espacios en blanco al principio y al final de la cadena
+        .str.strip()
+        # Convierta el texto a minúsculas
+        .str.lower()
+        # Transforme palabras que pueden (o no) contener guiones por su version sin guion.
+        .str.replace("-", "")
+        # Remueva puntuación y caracteres de control
+        .str.translate(str.maketrans("", "", "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"))
+        # Convierta el texto a una lista de tokens
+        .str.split()
+        # Transforme cada palabra con un stemmer de Porter
+        .apply(lambda x: [nltk.PorterStemmer().stem(w) for w in x])
+        # Ordene la lista de tokens y remueve duplicados
+        .apply(lambda x: sorted(set(x)))
+        # Convierta la lista de tokens a una cadena de texto separada por espacios
+        .str.join(" ")
+    )
+    
     """Con este paso, se crean los n-gramas en la columna key"""
     # Copie la columna 'text' a la columna 'key'
     df["key"] = df["text"]
@@ -39,30 +63,7 @@ def create_key(df, n):
         .apply(lambda x: sorted(set(x)))
         # Convierta la lista de ngramas a una cadena
         .str.join("")
-    )
-
-    """Con este paso, se aplica el algoritmo de Porter para encontrar la raíz de cada palabra"""
-    # Copie la columna 'text' a la columna 'stem'
-    df["stem"] = df["text"]
-    df["stem"] = (
-        df["stem"]
-        # Remueva los espacios en blanco al principio y al final de la cadena
-        .str.strip()
-        # Convierta el texto a minúsculas
-        .str.lower()
-        # Transforme palabras que pueden (o no) contener guiones por su version sin guion.
-        .str.replace("-", "")
-        # Remueva puntuación y caracteres de control
-        .str.translate(str.maketrans("", "", "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"))
-        # Convierta el texto a una lista de tokens
-        .str.split()
-        # Transforme cada palabra con un stemmer de Porter
-        .apply(lambda x: [nltk.PorterStemmer().stem(w) for w in x])
-        # Ordene la lista de tokens y remueve duplicados
-        .apply(lambda x: sorted(set(x)))
-        # Convierta la lista de tokens a una cadena de texto separada por espacios
-        .str.join(" ")
-    )
+    ) 
 
     return df
 
@@ -71,18 +72,21 @@ def generate_cleaned_column(df):
     """Crea la columna 'cleaned' en el DataFrame"""
 
     df = df.copy()
-
-    # Ordene el dataframe por 'key' y 'text'
-    df = df.sort_values(by=["key", "text"]).copy()
+    
+    # Ordene el dataframe por 'stem' y 'text'
+    df = df.sort_values(by=["stem", "text"]).copy()
     # Seleccione la primera fila de cada grupo de 'stem'
-    keys = df.groupby("stem").first().reset_index()
+    stem = df.groupby("stem").first().reset_index()
     # Cree un diccionario con 'stem' como clave y 'text' como valor
-    keys = keys.set_index("stem")["text"].to_dict()
-    # Cree la columna 'cleaned' usando el diccionario
-    df["cleaned"] = df["stem"].map(keys)
+    stem = stem.set_index("stem")["text"].to_dict()
 
-    """La columna stem no se requiere en el archivo final, por lo que procede a ser eliminada"""
+    # Cree la columna 'cleaned' usando el diccionario
+    df["cleaned"] = df["stem"].map(stem)
+    
+    # La columna stem no se requiere en el archivo final, por lo que procede a ser eliminada
     df.drop("stem", axis=1, inplace=True)
+    # Ordene el dataframe por 'key' y 'text'
+    df = df.sort_values(by=["key", "text"])
 
     return df
 
